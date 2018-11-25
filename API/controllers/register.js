e6dec9c897ee9a38fd25f12d16485a0106ef5769
Prem Kumar <prmsrswt@gmail.com>
2018-11-26 05:04:35 +0530

    RE: added node mailer

--- API/controllers/register.js
+++ API/controllers/register.js
@@ -13,7 +13,7 @@ const sendEmail = () => {
     });
     transporter.verify(function(error, success) {
 	   if (error) {
-	        console.log(error);
+	        console.log('miserable', error);
 	   } else {
 	        console.log('Server is ready to take our messages');
 
@@ -96,6 +96,7 @@ const handleRegister = (req,res, db, bcrypt) =>{
 					})
 					.then(() => {
 						res.status(200).json('Sucessfully Registered');
+						sendEmail();
 					})
 					.then(trx.commit)
 				}).catch(trx.rollback)
