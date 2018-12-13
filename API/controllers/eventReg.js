acc5442a2fb43ee671de90a82b6e2e183f584c68
Ojaswa Sharma <ojaswa1942@gmail.com>
2018-12-14 01:02:59 +0530

    Created endpoint controller for event registration

--- API/controllers/eventReg.js
+++ API/controllers/eventReg.js
@@ -1,5 +1,5 @@
 
-const handleEventReg = (req,res, db, bcrypt, xss) =>{
+const handleEventReg = (req, res, db, xss) =>{
 	const xssOptions = {
 		whiteList: [],
 		stripIgnoreTag: [],
@@ -8,56 +8,60 @@ const handleEventReg = (req,res, db, bcrypt, xss) =>{
 
 	const init = req.body.init;
 	const eid = req.body.eid;
-	const ifid = xss(req.body.ifid)
+	const ifid = xss(req.body.ifid);
+	let teamid;
 	if(init){
-		const email = req.email;
-		const teamid = ifid+'-'+eid;
+		// const email = req.email;
+		teamid = ifid+'-'+eid;
 	}
 	else {
-		const teamid = req.body.teamid;
+		teamid = req.body.teamid;
 	}
 
-	if(!ifid || !eid || !teamid)
+	if(!ifid || !eid || !teamid){
 		return res.status(400).json('Incorrect form submission');
-
-	db.select('*').from('users').where({ifid}).
-	then((data) =>{
-		if(!data.length)
-			return res.status(400).json('User does not exist!');
-		else{
-			db.transaction(trx=>{
-				return trx.insert({
-					email: email,
-					password: hash
-				})
-				.into('credentials')
-				.then(() => {
-					return trx('verify')
-					.insert({
-						email: email,
-						hash: verifyHash
-					})
-					.then(() =>{
-						return trx('users')
-						.insert({
-							email: email, 
-							name: name,
-							college: college,
-							gender: gender,
-							city: city,
-							mobile: phone,
-							ifid: eyeFID
+	}
+	
+	db.select('*').from('event_reg').where({ifid, eid})
+	.then(reges =>{
+		if(reges.length)
+			return res.status(400).json('Already registered!');
+		else {
+			db.select('*').from('users').where({ifid})
+			.then((data) =>{
+				if(!data.length)
+					return res.status(400).json('User does not exist!');
+				else{
+					db.transaction(trx=>{
+						return trx.insert({
+							ifid: ifid,
+							eid: eid,
+							teamid: teamid
 						})
+						.into('event_reg')
 						.then(() => {
-							res.status(200).json('Sucessfully Registered');
-							sendEmail(name, email, verifyHash, eyeFID);
+							return trx('payment')
+							.insert({
+								teamid: teamid,
+								status: 0
+							})
+							.then(() =>{
+								db('event_reg')
+								.join('payment', 'event_reg.teamid', '=', 'payment.teamid')
+								.join('events', 'events.eid', '=', 'event_reg.eid')
+								.select('event_reg.eid', 'events.ename', 'events.category', 'payment.teamid', 'payment.status')
+								.where('event_reg.ifid', '=', ifid)
+								.then(registrations =>{
+									res.status(200).json(registrations);
+								})
+								.then(trx.commit)
+							}).catch(trx.rollback)
 						})
-						.then(trx.commit)
-					}).catch(trx.rollback)
-				})
-				.catch(trx.rollback)
-			})
-			.catch(err => {console.log(err); res.status(400).json('Something went wrong!')});
+						.catch(trx.rollback)
+					})
+					.catch(err => {console.log(err); res.status(400).json('Something went wrong!')});
+				}
+			});
 		}
 	});
 }
