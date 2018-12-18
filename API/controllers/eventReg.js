5ea4ebc911886dc4fdbe7119d48dc007c41f3bfd
Ojaswa Sharma <ojaswa1942@gmail.com>
2018-12-18 13:10:00 +0530

    Modified controller to not give exception

--- API/controllers/eventReg.js
+++ API/controllers/eventReg.js
@@ -31,23 +31,22 @@ const handleEventReg = (req, res, db, xss) =>{
 		return res.status(400).json('Incorrect form submission');
 	}
 	
-	db.select('*').from('event_reg').where({ifid, eid})
-	.then(reges =>{
-		if(reges.length)
-			return res.status(400).json('Already registered!');
-		else {
-			db.select('*').from('users').where({ifid})
-			.then((data) =>{
-				if(!data.length)
-					return res.status(400).json('User does not exist!');
-				else{
-					db.transaction(trx=>{
-						return trx.insert({
+	db.transaction(trx => {
+		trx.select('*').from('event_reg').where({ifid, eid})
+		.then(reges =>{
+			if(reges.length)
+				return res.status(400).json('Already registered!');
+			else{
+				trx('users').select('*').where({ifid})
+				.then(data => {
+					if(!data.length)
+						return res.status(400).json('User does not exist!');
+					else{
+						return trx('event_reg').insert({
 							ifid: ifid,
 							eid: eid,
 							teamid: teamid
 						})
-						.into('event_reg')
 						.then(() => {
 							if(init){
 								return trx('payment')
@@ -55,38 +54,59 @@ const handleEventReg = (req, res, db, xss) =>{
 									teamid: teamid,
 									status: 0
 								})
-							}
-							else return Promise.resolve('1')
-						})
-						.then(() =>{
-							trx('event_reg')
-							.join('payment', 'event_reg.teamid', '=', 'payment.teamid')
-							.join('events', 'events.eid', '=', 'event_reg.eid')
-							.select('event_reg.eid', 'events.ename', 'events.category', 'payment.teamid', 'events.fee', 'payment.status')
-							.where('event_reg.ifid', '=', ifidUser)
-							.then(registrations =>{
-								var subquery = trx('event_reg').where({ifid: ifidUser}).select('teamid');
-								trx('event_reg')
-								.join('users','users.ifid', '=', 'event_reg.ifid')
-								.select('event_reg.eid', 'users.ifid', 'users.name', 'event_reg.teamid')
-								.where('event_reg.teamid', 'in', subquery)
-								.then(teamData =>{
-									let eventData = {
-										userEventReg: registrations,
-										userTeams: teamData,
-									}
-									res.status(200).json(eventData);
+								.then(() =>{
+									trx('event_reg')
+									.join('payment', 'event_reg.teamid', '=', 'payment.teamid')
+									.join('events', 'events.eid', '=', 'event_reg.eid')
+									.select('event_reg.eid', 'events.ename', 'events.category', 'payment.teamid', 'events.fee', 'payment.status')
+									.where('event_reg.ifid', '=', ifidUser)
+									.then(registrations =>{
+										var subquery = trx('event_reg').where({ifid: ifidUser}).select('teamid');
+										trx('event_reg')
+										.join('users','users.ifid', '=', 'event_reg.ifid')
+										.select('event_reg.eid', 'users.ifid', 'users.name', 'event_reg.teamid')
+										.where('event_reg.teamid', 'in', subquery)
+										.then(teamData =>{
+											let eventData = {
+												userEventReg: registrations,
+												userTeams: teamData,
+											}
+											res.status(200).json(eventData);
+										})
+										.then(trx.commit)
+									})
 								})
-								.then(trx.commit)
-							})
+							}
+							else {
+								trx('event_reg')
+								.join('payment', 'event_reg.teamid', '=', 'payment.teamid')
+								.join('events', 'events.eid', '=', 'event_reg.eid')
+								.select('event_reg.eid', 'events.ename', 'events.category', 'payment.teamid', 'events.fee', 'payment.status')
+								.where('event_reg.ifid', '=', ifidUser)
+								.then(registrations =>{
+									var subquery = trx('event_reg').where({ifid: ifidUser}).select('teamid');
+									trx('event_reg')
+									.join('users','users.ifid', '=', 'event_reg.ifid')
+									.select('event_reg.eid', 'users.ifid', 'users.name', 'event_reg.teamid')
+									.where('event_reg.teamid', 'in', subquery)
+									.then(teamData =>{
+										let eventData = {
+											userEventReg: registrations,
+											userTeams: teamData,
+										}
+										res.status(200).json(eventData);
+									})
+									.then(trx.commit)
+								})
+							}
 						})
 						.catch(trx.rollback)
-					})
-					.catch(err => {console.log(err); res.status(400).json('Something went wrong!')});
-				}
-			});
-		}
-	});
+					}
+				})
+			}
+		})
+		.catch(trx.rollback)		
+	})
 }
 
 module.exports = {
