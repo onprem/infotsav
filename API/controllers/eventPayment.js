a4703dd2bcd892d8e1c877ddea3f1a327fab1ed3
Prem Kumar <prmsrswt@gmail.com>
2018-12-26 17:38:00 +0530

    bugfix: Fee is now verifed at server side

--- API/controllers/eventPayment.js
+++ API/controllers/eventPayment.js
@@ -6,37 +6,49 @@ const handleEventPayment = (req,res, db) =>{
 	const ifid = req.body.ifid;
 	const teamid = req.body.teamid;
 	const fee = req.body.fee;
+	var feeDB = 500;
 	if(!ifid || !eid || !teamid || !fee){
 		return res.status(400).json('Incorrect form submission');
 	}
-	const orderid = teamid+'-'+randomstring.generate({length:3, charset:'numeric'});
 
-	paytmIndex.initPayment(fee, orderid, ifid).then(
-        success => {
-            console.log("Chala");
-            console.log(success);
-            db.transaction(trx=>{
-				return trx.insert({
-					orderid: orderid, 
-					teamid: teamid,
-					txnid: '0',
-					custid: ifid,
-					status: 'WAITING',
-					fee: fee,
-					txndate: '0'
-				})
-				.into('paytm')
-				.then(() => {
-					res.send(success);
-				})
-				.then(trx.commit)
-			}).catch(err => {console.log(err); res.status(400).json('Something went wrong!')});
-        },
-        error => {
-            console.log("nhi Chala");
-            res.send(error);
-        }
-    );
+	db.select('*').from('events').where({eid: eid})
+	.then(resp => {
+		feeDB = resp[0].fee;
+		if(feeDB > fee){
+			console.log(feeDB+' > '+fee);
+			return res.status(402).json('No money for ya');
+		}
+		else {
+			const orderid = teamid+'-'+randomstring.generate({length:3, charset:'numeric'});
+			paytmIndex.initPayment(fee, orderid, ifid).then(
+		        success => {
+		            console.log("Chala");
+		            console.log(success);
+		            db.transaction(trx=>{
+						return trx.insert({
+							orderid: orderid, 
+							teamid: teamid,
+							txnid: '0',
+							custid: ifid,
+							status: 'WAITING',
+							fee: fee,
+							txndate: '0'
+						})
+						.into('paytm')
+						.then(() => {
+							res.send(success);
+						})
+						.then(trx.commit)
+					}).catch(err => {console.log(err); res.status(400).json('Something went wrong!')});
+		        },
+		        error => {
+		            console.log("nhi Chala");
+		            res.send(error);
+		        }
+		    );
+		}
+	})
+	.catch(err => {console.log('eventPayment: ', err); res.status(400).json('Something is wrong');});	
 }
 module.exports = {
 	handleEventPayment: handleEventPayment
