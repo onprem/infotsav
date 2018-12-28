b4c37039a47a418338b36f48a581a94915f2f33c
Ojaswa Sharma <ojaswa1942@gmail.com>
2018-12-28 15:15:14 +0530

    Handle Easter redeemption

--- API/controllers/easter.js
+++ API/controllers/easter.js
@@ -7,45 +7,86 @@ const handleEasterRedeem = (req,res,db,xss)=>{
 	};
 	const egg = xss(req.body.egg, xssOptions);	
 	const email = req.email;
-	//1. Check if egg is valid - Invalid code! Try again?
-	//2. Fetch ifid from email
-	//Check if not already redeemed same egg - Code already redeemed
-	//3. Insert data into easter_redeem
-	//4. Reduce score by a fixed amount, if not less than some amount
-	//5. Send fresh score as response
-	// if(!email || !egg)
-	fetchScore(req, res, db);
-	console.log('Hi');
 
-		// throw('Nani?');
-	// db.transaction(trx =>{
-	//  	trx.select('*').from('verify').where({hash: hash})
-	// 	.then(verification_entry => {
-	// 		if(verification_entry.length){
-	// 			return trx('users').update({confirm: 1}).where({email: verification_entry[0].email})
-	// 				.then(() =>{
-	// 					return trx('verify')
-	// 					.where({
-	// 						email: verification_entry[0].email,
-	// 						hash: hash
-	// 					})
-	// 					.del()
-	// 					.then(()=> res.status(200).json('Email verification successfull!'))
-	// 					.then(trx.commit)
-	// 				})
-	// 				.catch(trx.rollback)
-	// 		}
-	// 		else
-	// 			res.status(302).redirect('https://www.infotsav.in/404');
-	// 	})
-	// 	.catch(trx.rollback)
-	// })
-	// .catch(err => res.status(400).json('Something is wrong'));
+	if(!email || !egg)
+		throw('Nani?');
+	db.transaction(trx =>{
+	 	trx.select('*').from('easter').where({egg})
+	 	.then(easterEgg => {
+	 		if(easterEgg.length){
+	 			trx('users').select('*').where({email})
+	 			.then(userData => {
+	 				const ifid = userData[0].ifid;
+	 				trx('easter_redeem').select('*').where({ifid, egg})
+	 				.then(userRedeem => {
+	 					if(userRedeem.length){
+	 						return res.status(400).json(`Egg already redeemed. Stop loading our servers!`);
+	 					}
+	 					else {
+	 						trx('easter_redeem')
+	 						.insert({
+	 							ifid, egg,
+	 							score: easterEgg[0].score
+	 						})
+	 						.then(() => {
+	 							if(easterEgg[0].score > 200){
+	 								trx('easter').where({egg}).decrement('score', 40)
+	 								.then(() => {
+	 									trx('easter_redeem')
+										.join('users', 'users.ifid', '=', 'easter_redeem.ifid')
+										.select('easter_redeem.ifid', 'name')
+										.sum({total: 'score'})
+										.groupBy('easter_redeem.ifid')
+										.orderBy('total', 'desc')
+										.limit(15)
+										.then((leaderboard) => {
+											trx('easter_redeem')
+											.sum({total: 'score'})
+											.where({ifid})
+											.then(userScore => {
+												const score = {userScore, leaderboard};
+												return res.status(200).json(score);
+											})
+											.then(trx.commit)
+										})
+	 								})
+	 							}
+	 							else {
+ 									trx('easter_redeem')
+									.join('users', 'users.ifid', '=', 'easter_redeem.ifid')
+									.select('easter_redeem.ifid', 'name')
+									.sum({total: 'score'})
+									.groupBy('easter_redeem.ifid')
+									.orderBy('total', 'desc')
+									.limit(15)
+									.then((leaderboard) => {
+										trx('easter_redeem')
+										.sum({total: 'score'})
+										.where({ifid})
+										.then(userScore => {
+											const score = {userScore, leaderboard};
+											return res.status(200).json(score);
+										})
+										.then(trx.commit)
+									})
+	 							}
+	 						})
+							.catch(trx.rollback)
+	 					}
+	 				})
+	 			})
+	 		}
+	 		else{
+	 			return res.status(400).json(`You got the wrong egg! Try again?`);
+	 		}
+	 	})
+	})
+	.catch(err => res.status(400).json('Something is wrong'));
 }
 
-const fetchScore = (req,res,db,emaill)=>{
-	const email = emaill || req.email;
-	db.select('*').from('users').where({email})
+const fetchScore = (req,res,db)=>{
+	const email = req.email;
+	return db.select('*').from('users').where({email})
 	.then(userInf => {
 		const ifid = userInf[0].ifid;
 		db('easter_redeem')
@@ -61,7 +102,7 @@ const fetchScore = (req,res,db,emaill)=>{
 			.where({ifid})
 			.then(userScore => {
 				const score = {userScore, leaderboard};
-				return res.status(200).json(score);			
+				return res.status(200).json(score);
 			})
 		})
 	})
