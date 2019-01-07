a1284229fbf62a946fa41f5a2c7909aba4da826f
Ojaswa Sharma <ojaswa1942@gmail.com>
2019-01-08 02:31:27 +0530

    Minor changes

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -43,15 +43,15 @@ class Nav extends Component {
 				}
 				<Link onClick={this.toggleNavView} to="/team">OUR TEAM</Link>
 				<Link onClick={this.toggleNavView} to="/contact">CONTACT US</Link>
-				
-				<div className='egg'>
+				<div>
 				  <Link onClick={this.toggleNavView} to="/easter">
-				  	<div className="egg_text">
-				  		FOUND AN EGG?
-				  	</div>
-				  </Link>  
-				</div>
-				
+					<div className='egg'>
+					  	<div className="egg_text">
+					  		FOUND AN EGG?
+					  	</div>
+					</div>
+				  </Link>
+			  	</div>  
 			</nav>
 		</div>
     );
