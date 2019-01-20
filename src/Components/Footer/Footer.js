1846f6b3a1a0d45b4631d56d6f51a88dfb187e9a
Prem Kumar <prmsrswt@gmail.com>
2019-01-21 01:06:28 +0530

    footer: added app's link

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,8 +5,9 @@ export const Footer = () => {
 		<div className="social colsoc">
 				<a className="soc" href="https://www.facebook.com/infotsav" target="_blank" rel="noopener noreferrer"><span className="fab fa-facebook-f"></span></a>
 				<a className="soc" href="https://www.instagram.com/infotsav19" target="_blank" rel="noopener noreferrer"><span className="fab fa-instagram"></span></a>
-				<a className="soc" href="https://twitter.com/infotsav" target="_blank" rel="noopener noreferrer"><span className="fab fa-twitter"></span></a>
+				<a className="soc" href="https://play.google.com/store/apps/details?id=com.infotsav.test" target="_blank" rel="noopener noreferrer"><span className="fab fa-google-play"></span></a>
 				<a className="soc" href="https://www.linkedin.com/in/infotsav-iiitm-9b9941174/" target="_blank" rel="noopener noreferrer"><span className="fab fa-linkedin-in"></span></a>
+				<a className="soc" href="https://twitter.com/infotsav" target="_blank" rel="noopener noreferrer"><span className="fab fa-twitter"></span></a>
 		</div>
 	);
 }
